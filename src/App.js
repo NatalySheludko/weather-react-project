38e955b7a45a686248b0e308ff23a62d4eb92790
NatalySheludko <137063086+NatalySheludko@users.noreply.github.com>
2023-11-13 20:02:41 +0200

    Added the GitHub repository URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        {" "}
+        This project was coded by Nataly Sheludko and is{" "}
+        <a
+          href="https://github.com/NatalySheludko/main-weather-project"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
